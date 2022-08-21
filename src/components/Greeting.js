8f26aada3947f53e8bee1a24e69afce8a633fddc
Flashmaxi <edorizvic2@gmail.com>
2022-08-21 13:22:47 +0200

    practice

---
+++ src/components/Greeting.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Output from "./Output";
+
+const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  }
+
+  return <div>
+    <h2>Hello World</h2>
+    {!changedText && <Output>It's good to see you!</Output>}
+    {changedText && <Output>Bye!</Output>}
+    <button onClick={changeTextHandler}>Change text!</button>
+  </div>
+};
+
+export default Greeting;
